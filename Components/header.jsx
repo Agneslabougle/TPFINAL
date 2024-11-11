df63e81f8b06de90dc29cd290d3dd68016177a9c
Sergio <principiante.en.programar@gmail.com>
2024-11-11 19:27:36 -0300

    change of text body and header

--- Components/header.jsx
+++ Components/header.jsx
@@ -1,9 +1,12 @@
 
-export default function Header(){
+export default function Header() {
 
-    return(
-        <h1>
-            Esto es un header
-        </h1>
+    return (
+        <div>
+            <video playsInline autoPlay loop muted>
+                <source src="/videos/hero-outside-desktop.mp4" type="video/mp4" />
+            </video>
+            <h1 className="absolute top-0 text-5xl m-20 ml-5">Cuidado del Reino Animal</h1>
+        </div>
     )
 }
